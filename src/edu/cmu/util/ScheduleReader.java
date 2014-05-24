01f944392178a7a9560d420164a665629734f95a
Celine Patag <chibeepatag@users.noreply.github.com>
2014-05-24 10:47:38 +0930

    why doesn't it reach tuesday?

--- src/edu/cmu/util/ScheduleReader.java
+++ src/edu/cmu/util/ScheduleReader.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +17,7 @@ import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.StartDocument;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
@@ -41,101 +43,137 @@ public class ScheduleReader {
 	static final String START = "start";
 	static final String END = "end";
 
-	public Schedule readSchedule(String url){
-		Schedule schedule = null;
-		Map<String, List<OpenSlot>> dayOpenSlotMap = null;
+	public Schedule readSchedule(String url) throws ScheduleXMLException {
+		Schedule schedule = null;		
 		try {
-		      // First, create a new XMLInputFactory
-		      XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-		      // Setup a new eventReader
-		      InputStream in = new FileInputStream(url);
-		      XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
-		      while(eventReader.hasNext()){
-		    	  XMLEvent event = eventReader.nextEvent();
-		    	  if(event.isStartDocument()){
-		    		  schedule = new Schedule();
-		    		 dayOpenSlotMap = schedule.getDayOpenSlotMap();
-		    	  }else{
-		    		  if(event.isStartElement()){
-		    			  StartElement startElementDayOfWeek = event.asStartElement();
-		    			  String dayOfWeek = startElementDayOfWeek.getName().getLocalPart();
-		    			  switch (dayOfWeek) {
-						case MONDAY:
-							List<OpenSlot> openSlots = dayOpenSlotMap.get(MONDAY);
-							Date[] time = readTimeSlot(eventReader);							
-							OpenSlot openSlot = new OpenSlot(time[0], time[1]);							
-							System.out.println(openSlot.getStart());
-							System.out.println(openSlot.getEnd());
-							break;
-						default:
-							break;
-						}
-		    		  }
-		    	  }
-		      }
-		}catch (FileNotFoundException fnf){
-			System.out.print("File "); 
-			System.out.print(url); 
+			// First, create a new XMLInputFactory
+			XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+			// Setup a new eventReader
+			InputStream in = new FileInputStream(url);
+			XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+			XMLEvent scheduleEvent = eventReader.nextTag();
+			if (scheduleEvent.isStartElement()) {
+				StartElement scheduleDoc = scheduleEvent.asStartElement();
+				if (scheduleDoc.getName().getLocalPart().equals(SCHEDULE)) {
+					schedule = new Schedule();		
+					readDaySchedule(eventReader, MONDAY, schedule.getDayOpenSlotMap().get(MONDAY));					
+					readDaySchedule(eventReader, TUESDAY, schedule.getDayOpenSlotMap().get(TUESDAY));
+				} else {
+					throw new ScheduleXMLException();
+				}
+			}
+
+			/*
+			 * while (eventReader.hasNext()) { XMLEvent event =
+			 * eventReader.nextTag(); if (event.isStartDocument()) { schedule =
+			 * new Schedule(); dayOpenSlotMap = schedule.getDayOpenSlotMap(); }
+			 * else { if (event.isStartElement()) { StartElement
+			 * startElementDayOfWeek = event .asStartElement(); String dayOfWeek
+			 * = startElementDayOfWeek.getName() .getLocalPart(); switch
+			 * (dayOfWeek) { case MONDAY: { List<OpenSlot> openSlots =
+			 * dayOpenSlotMap .get(MONDAY); openSlots =
+			 * readOpenSlots(eventReader); break; } case TUESDAY: {
+			 * List<OpenSlot> openSlots = dayOpenSlotMap .get(TUESDAY);
+			 * openSlots = readOpenSlots(eventReader); break; }
+			 * 
+			 * default: break; } } } }
+			 */
+		} catch (FileNotFoundException fnf) {
+			System.out.print("File ");
+			System.out.print(url);
 			System.out.println(" not found.");
-		}catch (XMLStreamException xmlStream){
-			
+		} catch (XMLStreamException xmlStream) {
+
 		}
-			
+
 		return schedule;
 	}
 
-	private Date[] readTimeSlot(XMLEventReader eventReader)
+	private void readDaySchedule(XMLEventReader eventReader,  String day,
+			List<OpenSlot> openSlots) throws XMLStreamException {
+		XMLEvent scheduleEvent = eventReader.peek();
+		while (!scheduleEvent.isStartElement() || !scheduleEvent.asStartElement().getName().getLocalPart()
+				.equals(day)) {
+			scheduleEvent = getNextOpenTag(eventReader);
+			System.out.println("Moving to next tag");
+		}
+		System.out.println("found" + scheduleEvent.asStartElement().getName().getLocalPart());
+		//call to read openslots
+
+	}
+
+	private List<OpenSlot> readOpenSlots(XMLEventReader eventReader)
+			throws XMLStreamException {
+		List<OpenSlot> openSlots = new ArrayList<>();
+		XMLEvent event = eventReader.nextTag();
+		if (event.isStartElement()) {
+			StartElement startElement = event.asStartElement();
+			if (startElement.getName().getLocalPart().equals(OPENSLOT)) {
+				Date[] startEnd = readStartEndTime(eventReader);
+				OpenSlot openSlot = new OpenSlot(startEnd[0], startEnd[1]);
+				openSlots.add(openSlot);
+			}
+		}
+		return openSlots;
+	}
+
+	private Date[] readStartEndTime(XMLEventReader eventReader)
 			throws XMLStreamException {
 		String startTime = null;
 		String endTime = null;
 		XMLEvent openSlotEvent = eventReader.nextEvent();
-		while(!openSlotEvent.isStartElement()){
+		while (!openSlotEvent.isStartElement()) {
 			openSlotEvent = eventReader.nextEvent();
 		}
-		if(openSlotEvent.isStartElement()){
-			StartElement startOpenSlotElement = openSlotEvent.asStartElement();
-			if(startOpenSlotElement.getName().getLocalPart().equals(OPENSLOT)){
-				openSlotEvent = eventReader.nextTag();								
-				if(openSlotEvent.isStartElement()){
-					StartElement startElement = openSlotEvent.asStartElement();
-					if(startElement.getName().getLocalPart().equals(START)){			
-						Characters chart = eventReader.nextEvent().asCharacters();
-						startTime = chart.toString();
-					}	
-				}								
-			
-				openSlotEvent = eventReader.nextTag();
-				while(!openSlotEvent.isStartElement()){
-					openSlotEvent = eventReader.nextEvent();
+		if (openSlotEvent.isStartElement()) {
+			if (openSlotEvent.isStartElement()) {
+				StartElement startElement = openSlotEvent.asStartElement();
+				if (startElement.getName().getLocalPart().equals(START)) {
+					Characters chart = eventReader.nextEvent().asCharacters();
+					startTime = chart.toString();
 				}
-			
-				if(openSlotEvent.isStartElement()){
-					StartElement startElement = openSlotEvent.asStartElement();
-					if(startElement.getName().getLocalPart().equals(END)){			
-						Characters chart = eventReader.nextEvent().asCharacters();
-						endTime = chart.toString();
-					}	
+			}
+
+			openSlotEvent = eventReader.nextTag();
+			while (!openSlotEvent.isStartElement()) {
+				openSlotEvent = eventReader.nextEvent();
+			}
+
+			if (openSlotEvent.isStartElement()) {
+				StartElement startElement = openSlotEvent.asStartElement();
+				if (startElement.getName().getLocalPart().equals(END)) {
+					Characters chart = eventReader.nextEvent().asCharacters();
+					endTime = chart.toString();
 				}
-			
-			
-			}								
-		}								
-		
+			}
+		}
+
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss");
-		
+
 		Date[] time = new Date[2];
 		try {
 			time[0] = simpleDateFormat.parse(startTime);
 			time[1] = simpleDateFormat.parse(endTime);
 		} catch (ParseException e) {
-			System.out.println(e.getMessage());			
+			System.out.println(e.getMessage());
 		}
 		return time;
 	}
 	
+	private XMLEvent getNextOpenTag(XMLEventReader eventReader) throws XMLStreamException{
+		XMLEvent xmlEvent = eventReader.peek();
+		while(!xmlEvent.isStartElement()){
+			xmlEvent = eventReader.nextTag();
+		}
+		return xmlEvent;
+	} 
 
 	public static void main(String[] args) {
 		ScheduleReader xmlReader = new ScheduleReader();
-		xmlReader.readSchedule("Schedules/schedule1.xml");
+		try {
+			xmlReader.readSchedule("Schedules/schedule1.xml");
+		} catch (ScheduleXMLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
---
+++ src/edu/cmu/util/ScheduleXMLException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package edu.cmu.util;
+
+/**
+ * This exception is thrown when no
+ * <schedule> tag is found in the given xml.
+ * @author Celine Patag
+ * 
+ */
+public class ScheduleXMLException extends Exception {
+	
+	public ScheduleXMLException() {
+		super("No schedule found.");
+	}
+}
