9687493f5cc6b8c09cf7705add3a49ffe02c4b3a
Celine Patag <chibeepatag@users.noreply.github.com>
2014-05-24 17:17:11 +0930

    urlList reader and schedule reader

--- src/edu/cmu/util/Scheduler.java
+++ src/edu/cmu/util/Scheduler.java
@@ -3,6 +3,9 @@
  */
 package edu.cmu.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Celine Patag
  *
@@ -15,7 +18,21 @@ public class Scheduler {
 	public static void main(String[] args) {
 		int duration = Integer.parseInt(args[0]);
 		System.out.println(duration);
+		
+		UrlListReader urlListReader = new UrlListReader();
+		List<String> urlList = urlListReader.readScheduleUrls("Schedules/urlList.xml");
+		
+		ScheduleReader scheduleReader = new ScheduleReader();
+		for(String url : urlList){
+			try {
+				scheduleReader.readSchedule(url);
+			} catch (ScheduleXMLException e) {
+				e.printStackTrace();
+			}			
+		}
+		
 
 	}
+	
 
 }
---
+++ src/edu/cmu/util/UrlListReader.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package edu.cmu.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+
+/**
+ * @author Celine Patag
+ *
+ */
+public class UrlListReader {
+
+	static final String URLLIST = "URLList";
+	static final String URL = "URL";
+	/**
+	 * This method reads the urlList.xml and produces a list
+	 * of urls of the schedules.
+	 * @param url of the xml containing the list of schedules
+	 * @return a list of urls of the schedules
+	 */
+	public List<String> readScheduleUrls(String url){
+		List<String> urlList = new ArrayList<String>();
+		
+		try {
+			XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+			InputStream in = new FileInputStream(url);
+			XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+			XMLEvent event = eventReader.nextTag();
+			if(event.isStartElement()){
+				String eventName = event.asStartElement().getName().getLocalPart();
+				if(eventName.equals(URLLIST)){
+					XMLEvent urlEvent = eventReader.nextTag();
+					while(eventReader.hasNext()){
+						
+						if(urlEvent.isStartElement() && urlEvent.asStartElement().getName().getLocalPart().equals(URL)){
+							urlEvent = eventReader.nextEvent();
+							urlList.add(urlEvent.asCharacters().getData());
+						}
+						urlEvent = eventReader.nextEvent();
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XMLStreamException xmlStream) {
+			xmlStream.printStackTrace();
+		}
+		
+		
+		return urlList;
+	}
+	
+	public static void main(String[] args) {
+		UrlListReader urlListReader = new UrlListReader();
+		urlListReader.readScheduleUrls("Schedules/urlList.xml");
+	}
+
+}
