34f33958306119eab805ad64d3f852bd83abd4d5
Celine Patag <chibeepatag@users.noreply.github.com>
2014-05-25 12:51:16 +0930

    TestGetCommonTime pass unit tests

--- test/edu/cmu/util/CommonTimeFinderTest.java
+++ test/edu/cmu/util/CommonTimeFinderTest.java
@@ -26,11 +26,13 @@ public class CommonTimeFinderTest {
 	Schedule schedule1;
 	Schedule schedule2;
 	
-	Date start1;
-	Date end1;
-	Date start2;
-	Date end2;	
-	Date end3;
+	Date time1;//9:30
+	Date time2;//10:30
+	Date time3;//9:30
+	Date time4;	//11:30
+	Date time5; //16:30
+	Date time6; //17:30
+	
 	
 	OpenSlot openSlot1;
 	OpenSlot openSlot2;
@@ -49,25 +51,28 @@ public class CommonTimeFinderTest {
 		
 		Calendar calendar1 = Calendar.getInstance();
 		calendar1.set(2014, 5, 25, 9, 30); //9:30 am
-		start1 = calendar1.getTime();		
+		time1 = calendar1.getTime();		
 		Calendar calendar2 = Calendar.getInstance();
 		calendar2.set(2014, 5, 25, 10, 30); //10:30 am
-		end1 = calendar2.getTime();
+		time2 = calendar2.getTime();
 
 		Calendar calendar3 = Calendar.getInstance();
 		calendar3.set(2014, 5, 25, 9, 30); //9:30 am
-		start2 = calendar3.getTime();		
+		time3 = calendar3.getTime();		
 		Calendar calendar4 = Calendar.getInstance();
 		calendar4.set(2014, 5, 25, 11, 30); //11:30 am
-		end2 = calendar4.getTime();
+		time4 = calendar4.getTime();
 		
 		Calendar calendar5 = Calendar.getInstance();
 		calendar5.set(2014, 5, 25, 16, 30);
-		end3 = calendar5.getTime(); //16:30
+		time5 = calendar5.getTime(); //16:30
 		
-		openSlot1 = new OpenSlot(start1, end1); //duration 1 hour 9:30-10:30
-		openSlot2 = new OpenSlot(start2, end2); //duration 2 hour 9:30 - 11:30
-		openSlot3 = new OpenSlot(end2, end3); //11:30 - 16:40
+		Calendar calendar6 = Calendar.getInstance();
+		calendar6.set(2014, 5, 25, 17, 30);
+		time6 = calendar6.getTime();
+		openSlot1 = new OpenSlot(time1, time2); //duration 1 hour 9:30-10:30
+		openSlot2 = new OpenSlot(time3, time4); //duration 2 hour 9:30 - 11:30
+		openSlot3 = new OpenSlot(time4, time5); //11:30 - 16:40
 		openSlots = new ArrayList<OpenSlot>();
 		openSlots.add(openSlot1);
 		openSlots.add(openSlot2);
@@ -95,10 +100,10 @@ public class CommonTimeFinderTest {
 	public void testRemoveShortOpenSlots(){
 		commonTimeFinder = new CommonTimeFinder(7200); //2 hours						
 
-		long duration = (end1.getTime() - start1.getTime())/(1000 * 3600);		
+		long duration = (time2.getTime() - time1.getTime())/(1000 * 3600);		
 		assertEquals(1, duration);
 		
-		duration = (end2.getTime() - start2.getTime())/(1000 * 3600);		
+		duration = (time4.getTime() - time3.getTime())/(1000 * 3600);		
 		assertEquals(2, duration);
 		
 		assertEquals(2, openSlots.size());
@@ -128,14 +133,35 @@ public class CommonTimeFinderTest {
 		assertTrue(!result2);
 		
 		//coinciding start2 and end1
-		OpenSlot openSlotEndCoincidesWithStart = new OpenSlot(end1, end2);
+		OpenSlot openSlotEndCoincidesWithStart = new OpenSlot(time2, time4);
 		boolean result3 = commonTimeFinder.checkIfTimeSlotCoincides(openSlot1, openSlotEndCoincidesWithStart);
 		assertFalse(result3);
 	}
 	
 	@Test
 	public void testGetCommonTimeOfDay(){
-		fail("Not yet implemented");
+		commonTimeFinder = new CommonTimeFinder(3600);
+		List<OpenSlot> openSlots1 = new ArrayList<OpenSlot>();
+		OpenSlot openSlot1 = new OpenSlot(time1, time2);
+		OpenSlot openSlot2 = new OpenSlot(time2, time4);
+		OpenSlot openSlot3 = new OpenSlot(time5, time6);
+		openSlots1.add(openSlot1);
+		openSlots1.add(openSlot2);
+		openSlots1.add(openSlot3);
+		
+		List<OpenSlot> openSlots2 = new ArrayList<OpenSlot>();
+		OpenSlot openSlot4 = new OpenSlot(time1, time4);
+		OpenSlot openSlot5 = new OpenSlot(time4, time6);		
+		openSlots2.add(openSlot4);
+		openSlots2.add(openSlot5);
+				
+		try {
+			OpenSlot commonSlot = commonTimeFinder.getCommonTimeOfDay(openSlots1, openSlots2);
+			System.out.println(commonSlot);
+		} catch (EmptyOpenSlotException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 }
